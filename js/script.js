e2cbf480bfad22e4c448e2f33566b35c4e5658f8
Soficius <103771506+Soficius@users.noreply.github.com>
2022-07-20 17:31:59 +0200

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -2,5 +2,23 @@ const root = new Vue ({
     el: '#root',
     data: {
         emails: [],
-    }
+    },
+    methods: {
+        getEmail(){
+            console.log('getEmail()')
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    console.log('response:',response);
+                    this.emails.push(response.data.response)
+                })
+                .catch((error) => {
+                    console.log('error:',error);
+                })
+        }
+    },
+    mounted() {
+        for(let i=0;i<10;i++){
+            this.getEmail()
+        }
+    },
 })
